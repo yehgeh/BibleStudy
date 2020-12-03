1d0fafd44be6efe0f528d34ba3e952687f04d79f
Annie Wang <zhiyewan@usc.edu>
2020-12-02 16:40:16 -0800

    added front end page

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 id="yehgehHeader"> YehGeh Bible Study </h1>
+        <h3 id="helloText"> Hello there!! :) This is our main page! </h3>
+      </header>
+        
+      <div>
+        <footer class="footer"> Copyright &copy; 2020. Andrew Hang & Annie Wang. </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
